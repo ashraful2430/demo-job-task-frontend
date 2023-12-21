bdedb326064c50522261e4575298cc7204929301
ashraful2430 <www.savage582@gmail.com>
2023-12-21 12:49:34 +0600

    added footer

--- src/Components/MainLayout/MainLayout.jsx
+++ src/Components/MainLayout/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 const MainLayout = () => {
   return (
     <div>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
